004d2e2625093d50ad4c75e24a578bce15fcb96e
Rama Diaz <ramadiaz221@gmail.com>
2024-11-27 11:57:37 +0700

    UPDATE of Users model

--- models/user_model.go
+++ models/user_model.go
@@ -8,15 +8,18 @@ import (
 
 type Users struct {
 	gorm.Model
-	ID              string `gorm:"type:uniqueidentifier;default:NEWID()"`
-	Name            string `gorm:"not null"`
-	Email           string `gorm:"unique;not null"`
-	HashedPassword  string `gorm:"not null"`
-	EmailVerifiedAt time.Time
-	PhoneNumber     string `gorm:"not null"`
-	Country         string `gorm:"not null"`
-	Province        string `gorm:"not null"`
-	City            string `gorm:"not null"`
-	ZipCode         string `gorm:"not null"`
-	CompleteAddress string `gorm:"not null"`
+	ID              string     `gorm:"type:uniqueidentifier;default:NEWID()"`
+	Name            string     `gorm:"not null"`
+	Email           string     `gorm:"unique;not null"`
+	HashedPassword  string     `gorm:"not null"`
+	EmailVerifiedAt *time.Time `gorm:"null;default:null"`
+	PhoneNumber     string     `gorm:"not null"`
+	Country         string     `gorm:"not null"`
+	Province        string     `gorm:"not null"`
+	City            string     `gorm:"not null"`
+	ZipCode         string     `gorm:"not null"`
+	CompleteAddress string     `gorm:"not null"`
+	CreatedAt       time.Time  `gorm:"null"`
+	UpdatedAt       time.Time  `gorm:"null"`
+	DeletedAt       *time.Time `gorm:"null;default:null"`
 }
