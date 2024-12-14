410d9b33c166a2a4d6e6f095e5a4d65c38ec24a4
Rama Diaz <ramadiaz221@gmail.com>
2024-12-15 06:58:35 +0700

    INIT of hotel_mapper

---
+++ mapper/hotel_mapper.go
@@ -0,0 +1,20 @@
+package mapper
+
+import (
+	"nganterin-go/dto"
+	"nganterin-go/models"
+
+	"github.com/go-viper/mapstructure/v2"
+)
+
+func MapHotelInputToModel(input dto.HotelInputDTO) models.Hotels {
+	var hotel models.Hotels
+	mapstructure.Decode(input, &hotel)
+	return hotel
+}
+
+func MapHotelModelToOutput(model models.Hotels) dto.HotelOutputDTO {
+	var hotelOutput dto.HotelOutputDTO
+	mapstructure.Decode(model, &hotelOutput)
+	return hotelOutput
+}
