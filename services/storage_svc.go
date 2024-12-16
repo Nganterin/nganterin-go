3220577e8b14e12efa20171e5f6f38d2ceb1ab3e
Rama Diaz <ramadiaz221@gmail.com>
2024-12-17 06:42:56 +0700

    UPDATE of SaveFileToDrive svc

--- services/storage_svc.go
+++ services/storage_svc.go
@@ -4,16 +4,19 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"os"
 
 	"google.golang.org/api/drive/v3"
 	"google.golang.org/api/option"
 )
 
-func (s *compServices) SaveFileToDrive(file []byte, name string, format string) (string, error) {
+func (s *compServices) SaveFileToDrive(file []byte, name string, format string) (*string, error) {
+	credentialsJSON := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+
 	ctx := context.Background()
-		driveService, err := drive.NewService(ctx, option.WithCredentialsFile("secret.json"))
+	driveService, err := drive.NewService(ctx, option.WithCredentialsJSON([]byte(credentialsJSON)))
 	if err != nil {
-		return "", errors.New("failed to create drive service: " + err.Error())
+		return nil, errors.New("failed to create drive service: " + err.Error())
 	}
 
 	fileMetadata := &drive.File{
@@ -27,7 +30,7 @@ func (s *compServices) SaveFileToDrive(file []byte, name string, format string) 
 		Media(fileReader).
 		Do()
 	if err != nil {
-		return "", errors.New("failed to upload file to drive: " + err.Error())
+		return nil, errors.New("failed to upload file to drive: " + err.Error())
 	}
 
 	_, err = driveService.Permissions.Create(uploadedFile.Id, &drive.Permission{
@@ -35,10 +38,10 @@ func (s *compServices) SaveFileToDrive(file []byte, name string, format string) 
 		Type: "anyone",
 	}).Do()
 	if err != nil {
-		return "", errors.New("failed to set file permissions: " + err.Error())
+		return nil, errors.New("failed to set file permissions: " + err.Error())
 	}
 
 	publicLink := "https://drive.google.com/file/d/" + uploadedFile.Id + "/view?usp=sharing"
 
-	return publicLink, nil
+	return &publicLink, nil
 }
