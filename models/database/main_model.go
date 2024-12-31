f5fc76a7d5ffe67f97439aa0a76bfc082ca06b56
Rama Diaz <ramadiaz221@gmail.com>
2025-01-01 00:11:53 +0700

    UPDATE of AllModels models.database

--- models/database/main_model.go
+++ models/database/main_model.go
@@ -1,5 +1,17 @@
 package database
 
 var AllModels = []interface{}{
+	&Client{},
+	&Files{},
 	&Users{},
+	&UserTokens{},
+	&Partners{},
+	&PartnerTokens{},
+	&Hotels{},
+	&HotelRooms{},
+	&HotelsLocation{},
+	&HotelPhotos{},
+	&HotelRoomPhotos{},
+	&HotelFacilities{},
+	&HotelOrders{},
 }
